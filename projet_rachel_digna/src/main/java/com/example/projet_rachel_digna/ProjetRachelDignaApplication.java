c19c743f6dfe31283c80b72d76de6f007dca94a0
Rachel DIGNA <rachel.digna@epita.fr>
2025-11-20 14:42:52 +0100

    feat: init project

---
+++ projet_rachel_digna/src/main/java/com/example/projet_rachel_digna/ProjetRachelDignaApplication.java
@@ -0,0 +1,13 @@
+package com.example.projet_rachel_digna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetRachelDignaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetRachelDignaApplication.class, args);
+    }
+
+}
---
+++ projet_rachel_digna/src/test/java/com/example/projet_rachel_digna/ProjetRachelDignaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projet_rachel_digna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetRachelDignaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
